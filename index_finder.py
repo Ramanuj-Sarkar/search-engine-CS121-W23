2c27d4f96555ede9ddbc9baa8d1f74edbb8a0a05
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2023-02-12 20:34:10 -0800

    Create index_finder.py
    I made this template which may or may not work.

---
+++ index_finder.py
@@ -0,0 +1,11 @@
+def fetch_posting(textfile): #fetches the elements to create the posting
+  return posting # what should posting be?
+
+
+def create_index(indexed_object):
+  index = {}
+  return index
+  
+
+if __name__ == '__main__':
+  create_index()

a6d37de9e6eec1a6fc18bb37b29f67395986ffb1
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2023-03-05 10:19:00 -0800

    Update indexer.py
    Added exact page match.

--- indexer.py
+++ indexer.py
@@ -6,49 +6,57 @@ from nltk.stem.snowball import SnowballStemmer
 from collections import defaultdict
 import pickle
 
+
 class indexer:
     def __init__(self) -> None:
-        self.doc_id = {} #will map doc_id to url
-        self.doc_id_gen  = 0 #value to assign to a doc
-        self.num_pages = 0 #number of indexed documents
-        self.inverted_index = {} #{token:{doc_id1: fequency, doc_id2: frequency}}
+        self.doc_id = {}  # will map doc_id to url
+        self.doc_id_gen = 0  # value to assign to a doc
+        self.num_pages = 0  # number of indexed documents
+        self.inverted_index = {}  # {token:{doc_id1: fequency, doc_id2: frequency}}
         self.unique_words = 0
         self.current_doc_id = 0
-    
+
     def create_index(self):
-        #opens up the directory with all the pages
+        # opens up the directory with all the pages
         target_pages = os.getcwd()
         target_pages += '\\DEV'
 
-        #gets all the pages and attaches it with correct path names
+        # gets all the pages and attaches it with correct path names
         for root, dirs, files in os.walk(target_pages):
+            # set used to find exact matches
+            exact_token_match = {}
             for file in files:
                 file = os.path.join(root, file)
-                #load the json file
+                # load the json file
                 file = self.load_json(file)
-                #file is now a json(dic format with 'url' 'content' 'encoding')
+                # file is now a json(dic format with 'url' 'content' 'encoding')
                 soup = BeautifulSoup(file['content'], "lxml")
-                self.num_pages += 1
-                #tokenize everything in the content json
+                # tokenize everything in the content json
                 tokens = self.tokenize(soup)
-                #tokenize important words
+                # find files that exactly match these tokens
+                checksum = sum(sum(ord(letter) for letter in token) for token in tokens)
+                if checksum in exact_token_match:
+                    continue
+                exact_token_match.add(checksum)
+                self.num_pages += 1
+                # tokenize important words
                 tokens_important = []
                 for i in soup.find_all(['b', 'strong', 'h1', 'h2', 'h3', 'title']):
-                    #tokenize everything that is within the tags above
+                    # tokenize everything that is within the tags above
                     tok = self.tokenize(i)
                     for i in tok:
                         tokens_important.append(i)
-                #stems the words
+                # stems the words
                 stem_tokens = self.stem_tokens(tokens)
                 stem_tokens_important = self.stem_tokens(tokens_important)
-                #get all the words in the page into a frequency dictionary
+                # get all the words in the page into a frequency dictionary
                 freq_dict = self.compute_word_frequencies(stem_tokens)
                 freq_dict_important = self.compute_word_frequencies(stem_tokens_important)
-                #add to index
+                # add to index
                 self.add_to_index(freq_dict, freq_dict_important)
 
     def add_to_index(self, freq_dict, freq_dict_important):
-        #loop through dict and add each token to inverted_index
+        # loop through dict and add each token to inverted_index
         for token in freq_dict.keys():
             if token in self.inverted_index:
                 self.inverted_index[token][self.current_doc_id] = freq_dict[token]
@@ -62,7 +70,7 @@ class indexer:
                     self.inverted_index[token][self.current_doc_id] += freq_dict_important[token]
 
     def stem_tokens(self, token_list):
-        #gets the tokenize list and uses stemming on all words
+        # gets the tokenize list and uses stemming on all words
         stemmer = SnowballStemmer("english")
         stem_words = []
         for token in token_list:
@@ -88,49 +96,50 @@ class indexer:
         for token in token_list:
             frequencies[token] += 1
         return frequencies
-                
+
     def load_json(self, file):
-        #opens json
+        # opens json
         j_file = json.load(open(file))
-        #assigns each file a doc id and maps it to the url
+        # assigns each file a doc id and maps it to the url
         self.assign_doc_id(j_file["url"])
         return j_file
 
     def assign_doc_id(self, file):
-        #maps the doc id to the url
+        # maps the doc id to the url
         self.doc_id[self.doc_id_gen] = file
         self.current_doc_id = self.doc_id_gen
         self.doc_id_gen += 1
-    
+
     def write_report(self):
-        #write stats to report
+        # write stats to report
         with open("report.txt", "w") as my_file:
             my_file.write("The number of indexed document: {}\n".format(self.num_pages))
             my_file.write("The number of unique words: {}\n".format(self.unique_words))
             my_file.write("The total size of index on disk: {} KB\n".format(self.get_index_file_size()))
 
     def pickle_index(self):
-        #stores inverted index in disk
+        # stores inverted index in disk
         with open("inverted_index.pickle", "wb") as my_file:
             pickle.dump(self.inverted_index, my_file)
-    
+
     def pickle_doc_id(self):
-        #stores inverted index in disk
+        # stores inverted index in disk
         with open("doc_id.pickle", "wb") as my_file:
             pickle.dump(self.doc_id, my_file)
 
     def get_index_file_size(self):
-        #get the path of pickle file
+        # get the path of pickle file
         file = os.getcwd()
         file += "\\inverted_index.pickle"
-        #returns the size
-        return os.path.getsize(file) / 1000 #to get in KB
+        # returns the size
+        return os.path.getsize(file) / 1000  # to get in KB
 
     def run(self):
         self.create_index()
         self.pickle_index()
         self.pickle_doc_id()
         self.write_report()
+
 
 if __name__ == "__main__":
     ini = indexer()
